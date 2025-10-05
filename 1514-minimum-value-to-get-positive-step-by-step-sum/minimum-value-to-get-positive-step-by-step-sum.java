6e2e05085afc736a428a419fac50584acf5b86fb
Hemanth <147475853+hemanthnethinti@users.noreply.github.com>
2025-10-06 01:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (65.69%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.java
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.java
@@ -2,12 +2,10 @@ class Solution {
     public int minStartValue(int[] nums) {
         int prefixSum = 0;
         int minPrefix = Integer.MAX_VALUE;
-
         for (int num : nums) {
             prefixSum += num;
             minPrefix = Math.min(minPrefix, prefixSum);
         }
-
         return minPrefix >= 0 ? 1 : 1 - minPrefix;
     }
 }
