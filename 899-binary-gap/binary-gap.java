05115473ab00e75a689ca63a73ec2f64a100c44d
Netinti Hemanth <hemanthnetinti@gmail.com>
2026-02-26 03:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (13.45%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int binaryGap(int n) {
+        int lastIndex = -1;
+        int maxDistance = 0;
+        int currentIndex = 0;
+        
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (lastIndex != -1) {
+                    maxDistance = Math.max(maxDistance, currentIndex - lastIndex);
+                }
+                lastIndex = currentIndex;
+            }
+            
+            n >>= 1;
+            currentIndex++;
+        }
+        
+        return maxDistance;
+    }
+}
