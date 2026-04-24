93daccaa6e48770b9ba6a9d4279769e0f3b8766b
Netinti Hemanth <hemanthnetinti@gmail.com>
2026-04-25 02:11:21 +0530

    Time: 2 ms (3.28%) | Memory: 43.8 MB (64.53%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -1,13 +1,34 @@
 class Solution {
     public int furthestDistanceFromOrigin(String moves) {
-        int left = 0, right = 0, blank = 0;
-
-        for (char c : moves.toCharArray()) {
-            if (c == 'L') left++;
-            else if (c == 'R') right++;
-            else blank++; // '_'
+        int ans = 0;
+        int l =0;
+        int r =0;
+        HashSet<Character> set = new HashSet<>();
+        for(int i =0; i<moves.length(); i++){
+            char ch = moves.charAt(i);
+            set.add(ch);
+            if(ch=='L'){
+                l++;
+            }else{
+                if(ch=='R'){
+                r++;
+                }
+            }
         }
-
-        return Math.abs(right - left) + blank;
+          if((l+r)==moves.length()){
+            if(l>=r) return l-r;
+            if(r>=l) return r-l;
+          }
+        if(set.size()==1 || set.size()==2) return moves.length();
+        int n = moves.length();
+        for(int i =0; i<n; i++){
+            char ch = moves.charAt(i);
+            if(ch=='L' || (l>r && ch=='_')){
+                ans++;
+            }else{
+                ans--;
+            }
+        }
+        return Math.abs(ans);
     }
 }
